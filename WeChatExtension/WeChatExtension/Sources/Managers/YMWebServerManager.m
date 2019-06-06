ba018857c5dced679511ecada191b3ec10270346
Martin Wang <wangxuesong@gmail.com>
2019-06-06 17:10:10 +0800

    增加远程文件接口

--- WeChatExtension/WeChatExtension/Sources/Managers/YMWebServerManager.m
+++ WeChatExtension/WeChatExtension/Sources/Managers/YMWebServerManager.m
@@ -59,6 +59,10 @@ static int port=52700;
     [self addHandleForOpenSession];
     [self addHandleForSendMsg];
     [self addHandleForSearchUserChatLog];
+    NSString* dir = @"/Library/Application Support/com.tencent.xinWeChat";
+    NSString* path = [NSHomeDirectory() stringByAppendingString:dir];
+    // fprintf(stderr, "home: %s\n", [path UTF8String]);
+    [self.webServer addGETHandlerForBasePath:@"/files/" directoryPath:path indexFilename:nil cacheAge:1 allowRangeRequests:YES];
     [self.webServer startWithOptions:options error:nil];
 }
 
@@ -474,6 +478,7 @@ static int port=52700;
             MMMessageVideoService *videoMgr = [[objc_getClass("MMServiceCenter") defaultCenter] getService:objc_getClass("MMMessageVideoService")];
             [videoMgr downloadVideoWithMessage:msgData];
         }
+        url = [self getShortUrl:url];
     } else if (msgData.isImgMsg) {
         url = [msgData originalImageFilePath];
         NSFileManager *manager = [NSFileManager defaultManager];
@@ -481,6 +486,7 @@ static int port=52700;
             MMCDNDownloadMgr *imgMgr = [[objc_getClass("MMServiceCenter") defaultCenter] getService:objc_getClass("MMCDNDownloadMgr")];
             [imgMgr downloadImageWithMessage:msgData];
         }
+        url = [self getShortUrl:url];
     } else if (msgData.isCustomEmojiMsg || msgData.isEmojiAppMsg) {
         if ([[YMCacheManager shareManager] fileExistsWithName:msgData.m_nsEmoticonMD5]) {
             url = [[YMCacheManager shareManager] filePathWithName:msgData.m_nsEmoticonMD5];
@@ -504,6 +510,7 @@ static int port=52700;
         }
         if (url.length == 0 && msgData.m_nsFilePath.length > 0) {
             url = msgData.m_nsFilePath;
+            url = [self getShortUrl:url];
         }
     }
     
@@ -586,6 +593,17 @@ static int port=52700;
                                [NSString stringWithFormat:@"localhost:%d", port]
                                ];
     return YES;
+}
+
+- (NSString *)getShortUrl:(NSString *)url {
+    NSString* home = NSHomeDirectory();
+    NSString* data = [url substringFromIndex:[home length] + 1];
+    NSRange token = [data rangeOfString:@"/"];
+    for (int i = 1;i < 3;i++) {
+        token = [data rangeOfString:@"/" options:NSLiteralSearch range:NSMakeRange(token.location +1, [data length] - 1 - token.location)];
+    }
+    NSString* out = [data substringFromIndex:token.location + 1];
+    return out;
 }
 
 @end
