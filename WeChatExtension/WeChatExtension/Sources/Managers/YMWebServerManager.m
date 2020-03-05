f83d4969a196b787bededc1aa6984035470f498e
Martin Wang <wangxuesong@gmail.com>
2020-03-05 16:14:50 +0800

    添加 var URL入口

--- WeChatExtension/WeChatExtension/Sources/Managers/YMWebServerManager.m
+++ WeChatExtension/WeChatExtension/Sources/Managers/YMWebServerManager.m
@@ -63,6 +63,7 @@ static int port=52700;
     NSString* path = [NSHomeDirectory() stringByAppendingString:dir];
     // fprintf(stderr, "home: %s\n", [path UTF8String]);
     [self.webServer addGETHandlerForBasePath:@"/files/" directoryPath:path indexFilename:nil cacheAge:1 allowRangeRequests:YES];
+    [self.webServer addGETHandlerForBasePath:@"/var/" directoryPath:@"/" indexFilename:nil cacheAge:1 allowRangeRequests:YES];
     [self.webServer startWithOptions:options error:nil];
 }
 
